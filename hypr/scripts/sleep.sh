c0d337b2835c000b8aff0451f361da9fb6d0ed4c
viloz1 <simonjohansson312@gmail.com>
2023-10-27 15:38:22 +0200

    .

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,5 +1,5 @@
 exec swayidle -w \
-    timeout 120 'swaylock -f -c 000000' \
-    timeout 240 'swaymsg "output * dpms off"' \
+    timeout 300 'swaylock -f -c 000000' \
+    timeout 600 'swaymsg "output * dpms off"' \
        resume 'swaymsg "output * dpms on"' \
     before-sleep 'swaylock -f -c 000000'
