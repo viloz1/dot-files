9a2ae49c27f00ad6a677047a0c725cf471b3284e
viloz1 <simonjohansson312@gmail.com>
2023-11-11 13:36:15 +0100

    fix mason

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,22 +4,59 @@ local default_plugins = {
 
   "nvim-lua/plenary.nvim",
   {
-    "sbdchd/neoformat",
+    "mhartington/formatter.nvim",
     lazy = false,
-    init = function ()
-      vim.g.neoformat_try_node_exe = 1
+    config = function()
+      local util = require "formatter.util"
+      require("formatter").setup {
+      -- Enable or disable logging
+      logging = true,
+      -- Set the log level
+      log_level = vim.log.levels.WARN,
+      -- All formatter configurations are opt-in
+      filetype = {
+        -- Formatter configurations for filetype "lua" go here
+        -- and will be executed in order
+          typescriptreact = {
+          -- "formatter.filetypes.lua" defines default configurations for the
+          -- "lua" filetype
+          require("formatter.filetypes.typescriptreact").prettier,
+
+          -- You can also define your own configuration
+          function(parser)
+            -- Full specification of configurations is down below and in Vim help
+            -- files
+            return {
+              exe = "prettier",
+              args = {
+                "--stdin-filepath",
+                util.escape_path(util.get_current_buffer_file_path()),
+                "--parser",
+                parser,
+              },
+              stdin = true,
+              try_node_modules = true,
+            }
+          end
+        },
+
+        -- Use the special "*" filetype for defining formatter configurations on
+        -- any filetype
+        ["*"] = {
+          -- "formatter.filetypes.any" defines default configurations for any
+          -- filetype
+          require("formatter.filetypes.any").remove_trailing_whitespace
+        }
+      }
+    }
     end,
-    opts = {
-      completion = {
-        autocomplete = false,
-      },
-    },
+
   },
   {
     "mfussenegger/nvim-lint",
     lazy = false,
   },
-  
+
   {
     "NvChad/base46",
     branch = "v2.0",
@@ -27,7 +64,7 @@ local default_plugins = {
       require("base46").load_all_highlights()
     end,
   },
-  
+
   {
     "NvChad/ui",
     branch = "v2.0",
@@ -288,7 +325,7 @@ local default_plugins = {
       dofile(vim.g.base46_cache .. "whichkey")
       require("which-key").setup(opts)
     end,
-  }, 
+  },
 }
 
 local config = require("core.utils").load_config()
