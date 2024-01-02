2c071204ab0efc5344c27692d3c89efc48c436d7
viloz1 <simonjohansson312@gmail.com>
2024-01-02 12:44:50 +0100

    fix swayidle again

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -2,4 +2,5 @@ exec swayidle -w \
     timeout 300 'swaylock -f -c 000000' \
     timeout 600 'swaymsg "output * dpms off"' \
        resume 'swaymsg "output * dpms on"' \
-    before-sleep 'swaylock -f -c 000000'
+    before-sleep 'swaylock -f -c 000000' \
+    before-sleep 'playerctl pause'
