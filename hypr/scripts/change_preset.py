daaa8717a99dd9697be1f6e09f580d05baf5fe32
viloz1 <simonjohansson312@gmail.com>
2024-01-09 08:34:37 +0100

    fix python and improve script

--- hypr/scripts/change_preset.py
+++ hypr/scripts/change_preset.py
@@ -4,37 +4,37 @@ import os.path
 import pathlib
 from gi.repository import Notify, GdkPixbuf
 
-cmd_output = subprocess.check_output(["easyeffects", "-p"]).decode("utf-8") 
-presets = cmd_output.removeprefix("Output Presets: ").split("\n", 1)[0].split(",")[:-1]
+cmd_output = subprocess.check_output(["easyeffects", "-p"]).decode("utf-8")
+home = str(pathlib.Path.home())
 
-path = pathlib.Path(str(pathlib.Path.home())+"/.var/current_preset")
-pathlib.Path(str(pathlib.Path.home())+"/.var").mkdir(parents=True, exist_ok=True)
+presets = cmd_output.removeprefix("Output Presets: ")\
+ .split("\n", 1)[0].split(",")[:-1]
+
+path = pathlib.Path(home+"/.var/current_preset")
+pathlib.Path(home+"/.var")\
+    .mkdir(parents=True, exist_ok=True)
 
 current_index = 0
 Notify.init("Presets change")
 
-if (not os.path.exists(path)):
-    with open(path, 'w') as f:
-        f.write(str(current_index))
-        
-else:
+if (os.path.exists(path)):
     with open(path, "r") as f:
         lines = f.readlines()
     current_index = (int(lines[0]) + 1) % len(presets)
-    
 
+with open(path, 'w') as f:
+    f.write(str(current_index))
 
-    with open(path, "w") as f:
-        f.write(str(current_index))
-
-print(current_index)
 subprocess.call(["easyeffects", "-l", str(presets[current_index])])
 
-image = GdkPixbuf.Pixbuf.new_from_file(str(pathlib.Path.home())+"/.config/hypr/scripts/easyeffects.svg")
-notification = Notify.Notification.new("Changed music preset", "New preset: {new}".format(new=presets[current_index]))
+image = GdkPixbuf.Pixbuf\
+    .new_from_file(home+"/.config/hypr/scripts/easyeffects.svg")
+
+notification = Notify.Notification\
+    .new("Changed music preset", "New preset: {new}"
+    .format(new=presets[current_index]))
 
 notification.set_icon_from_pixbuf(image)
 notification.set_image_from_pixbuf(image)
 
 notification.show()
-
