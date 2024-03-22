33d9c0651133ca8f6a4f338db96a616507774ccf
Simon Johansson <simon.johansson@satcube.com>
2024-03-22 14:29:46 +0100

    update set-hypr

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "chadracula",
-  theme_toggle = { "chadracula", "ayu_light" },
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "ayu_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
